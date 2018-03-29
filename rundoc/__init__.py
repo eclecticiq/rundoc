ddb2eb052cc5f817a78faecef746144f49033bdd
Predrag Mandic <predrag@phlogisto.com>
2018-03-29 15:11:27 +0200

    changed version to 0.1.17

--- rundoc/__init__.py
+++ rundoc/__init__.py
@@ -2,7 +2,7 @@
 Tool that let's you run code blocks from a markdown file in controlled manner.
 """
 
-__version__ = "0.1.15"
+__version__ = "0.1.17"
 __licence__ = "BSD"
 __year__ = "2017"
 __author__ = "EclecticIQ"
